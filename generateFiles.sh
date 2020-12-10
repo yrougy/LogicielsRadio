126e8ba67514e595b45183ea832c4e4054d35838
Yves <yrougy@gmail.com>
2020-12-10 17:37:11 +0100

    Première version

---
+++ generateFiles.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# We generate the Windows list
+echo -n "Generating Windows list"
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $2 == "Windows" || $2 =="Multiplateforme" ) print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsWindows.txt
+echo .
+
+# We generate the Linux list
+echo -n "Generating Linux list"
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $2 == "Linux" || $2 =="Multiplateforme" ) print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsLinux.txt
+echo .
+
+# We generate the MacOS list
+echo -n "Generating MacOS list"
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $2 == "MacOS" || $2 =="Multiplateforme" ) print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsMacOS.txt
+echo .
