e6a0bffad098c057d3fa4dbecfce350dffe62d0a
Yves <yrougy@gmail.com>
2020-12-11 11:32:37 +0100

    Colonnes pour les système d'exploitation ajoutées Meilleur tri par système d'exploitation Ajout de Android

--- generateFiles.sh
+++ generateFiles.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 
+#
+# Colonne Linux: 8
+# Colonne Windows: 9
+# Colonne MacOS: 10
+# Colonne Android: 11
+#
+#
+
 # We generate the full list
 echo -n "Generating Full list"
 sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > Logiciels.txt
 echo .
 
-
 # We generate the Windows list
 echo -n "Generating Windows list"
-sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $2 == "Windows" || $2 =="Multiplateforme" ) print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsWindows.txt
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $9 == "X" ) print $1, "\n\tSystème: Windows\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsWindows.txt
 echo .
 
 # We generate the Linux list
 echo -n "Generating Linux list"
-sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $2 == "Linux" || $2 =="Multiplateforme" ) print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsLinux.txt
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $8 == "X" ) print $1, "\n\tSystème: Linux\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsLinux.txt
 echo .
 
 # We generate the MacOS list
 echo -n "Generating MacOS list"
-sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $2 == "MacOS" || $2 =="Multiplateforme" ) print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsMacOS.txt
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $10 == "X" ) print $1, "\n\tSystème: MacOS\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsMacOS.txt
+echo .
+
+# We generate the Android list
+echo -n "Generating Android list"
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{if ( $11 == "X" ) print $1, "\n\tSystème: Android\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > LogicielsAndroid.txt
 echo .
