a42a0c99e4cf6e845788dc4f064a3da03f0a3542
Yves <yrougy@gmail.com>
2020-12-12 16:46:15 +0100

    Ajout de logiciels Exclusion de la ligne de titre du fichier original de la liste de logiciels Correction de typo pour Welle.io (aerogus)

--- generateFiles.sh
+++ generateFiles.sh
@@ -10,7 +10,7 @@
 
 # We generate the full list
 echo -n "Generating Full list"
-sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| awk -F, '{print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > Logiciels.txt
+sort --ignore-case -t, -k1,1 Logiciels\ Radio\ -\ Sheet1.csv| grep -v "^Nom" |  awk -F, '{print $1, "\n\tSystème:",$2,"\n\tFonction:",$3,"\n\tLicence:",$4,"\n\tPrix:",$5,"\n\tURL:",$6,"\n\tÉtat:",$7 ;} ' > Logiciels.txt
 echo .
 
 # We generate the Windows list
